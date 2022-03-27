1894196f12363de4542959af887e4ac8cdad2105
ghost171 <selevenko45@mail.ru>
2022-03-27 15:27:38 +0300

    Putting in the directory

---
+++ CMakeFiles/BinaryRenderer.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ghost17/Downloads/binary_rendering/External/Clock.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/External/Clock.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/External/ImageManager.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/External/ImageManager.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/External/SceneLoader.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/External/SceneLoader.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/Source/FrameBuffer.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/Source/FrameBuffer.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/Source/GP_ONE.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/Source/GP_ONE.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/Source/Sprite.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/Source/Sprite.cpp.o"
+  "/home/ghost17/Downloads/binary_rendering/Source/main.cpp" "/home/ghost17/Downloads/binary_rendering/CMakeFiles/BinaryRenderer.dir/Source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "External"
+  "Source"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Source/GP_ONE.cpp
@@ -0,0 +1,24 @@
+#include "GP_ONE.h"
+
+Sprite   GP_ONE::spriteMemory[MAX_SPRITE_COUNT];
+uint16_t GP_ONE::frameBuffer[FRAMEBUFFER_BUF_SIZE];
+
+
+void GP_ONE::loadSprites(const Sprite *sprites, uint16_t spriteCount) {
+
+}
+
+
+void GP_ONE::clearFrameBuffer(BackGroundColor bkgColor) {
+
+}
+
+
+void GP_ONE::drawSpriteInstances(const SpriteInstance *instances, uint16_t instanceCount) {
+
+}
+
+
+void GP_ONE::saveFrameBuffer(FrameBuffer &outFrameBuffer) {
+
+}
